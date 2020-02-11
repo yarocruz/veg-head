9ff7a0adc8d6b49fa385a53caff2ecb61751c03f
Alex Joel <alexjoeldelgado@gmail.com>
2020-02-10 21:35:27 -0500

    Renamed files, Incorporated Materialize, Stylized UI

--- recipesScript.js
+++ recipesScript.js
@@ -13,6 +13,9 @@ function getRecipe(){
             var newRecipeItem = $("<div class='wrapper col s4 card small'><div class='card-image waves-effect waves-block waves-light'><img class='activator' src='" + res.hits[i].recipe.image + "'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>" + res.hits[i].recipe.label + "<i class='material-icons right'>more_vert</i></span><p><a href='" + res.hits[i].recipe.url + "'>Click here for Recipe</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + res.hits[i].recipe.label + "<i class='material-icons right'>close</i></span><h6>What you will need:</h6><p>" +  + "</div></div>");
             // Small Card Element
             // $("<div class='wrapper'><div class='col-4'><div class='card small'><div class='card-image'><img class='responsive-image' src='" + res.hits[i].recipe.image + "' ><span class='card-title'>" + res.hits[i].recipe.label + "</span></div><div class='card-content'><p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p></div><div class='card-action'><a href='" + res.hits[i].recipe.url + "'>" + res.hits[i].recipe.url + "</a></div></div></div></div>");
+            
+            
+
             $("#recipeList").append(newRecipeItem);
         }
 })};
