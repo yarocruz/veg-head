38c7cb6ae787c6736e37e401250cebe5af636475
Michael Mendoza <michael.mend97@yahoo.com>
2020-02-11 15:52:32 -0500

    fixed pictures not loading with place holder img

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var zQueryUrl = 'https://developers.zomato.com/api/v2.1/search?lat=' + localStor
 
 let zomatoAPIKey = '4668fbe6f51718b42874da8ae396ea5c';
 
-$("button").click(function(){
+function search(){
     $.ajax(
         {
             type: "GET",
@@ -23,12 +23,12 @@ $("button").click(function(){
                 for (i = 0; i < 18; i++) {
                     
                 var srch = result.restaurants[i].restaurant;
-                    if (srch.photos[0].photo.url === null) {
-                        var pic = 'https://placekitten.com/200/200';
-                    } else {
-                        var pic = srch.photos[0].photo.url;
+                    if (srch.featured_image) {
+                        var pic = srch.featured_image;
+                    } else {    
+                        var pic = 'ph.jpg';
                     }
-                var rsts = $("<div class='wrapper col s4 card small'><div class='card-image waves-effect waves-block waves-light'><img class='activator' src='" + pic + "'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>more_vert</i></span><p><a href='" +  + "'>Click here for Menu</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>close</i></span><h6>More Info:</h6><p>" + "</div></div>");
+                var rsts = $("<div class='wrapper col s4 card small'><div class='card-image waves-effect waves-block waves-light'><img class='activator' src='" + pic + "'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>more_vert</i></span><p><a target='_blank' href='" + srch.menu_url + "'>Click here for Menu</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>close</i></span><h6>More Info:</h6><ul><h7>Street Address:</h7><li>" + srch.location.address + "</li><h7>Customer Rating:</h7><li>" + srch.user_rating.aggregate_rating + "</li><h7>Phone Number:</h7><li>" + srch.phone_numbers + "</li></ul>" + "</div></div>");
                 
                 $("#restaurants").append(rsts);
                 }
@@ -38,5 +38,16 @@ $("button").click(function(){
             },
         }
     );
+};
+
+search();
+
+$("button").click(function(){
+    search();
 });
-  
+$('#rstInput').keypress(function(e) {
+    var keycode = (e.keyCode);
+    if(keycode == '13'){
+        search();
+    }
+});
