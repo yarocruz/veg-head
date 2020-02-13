80abd8e82cf4ede3181388924a28ac32321ef146
yarocruz <yarocruz@gmail.com>
2020-02-12 20:51:04 -0500

    added simple map

--- assets/restaurantsScript.js
+++ assets/restaurantsScript.js
@@ -1,11 +1,12 @@
-function search(){
+function search() {
     if (rstInput.value === "") {
-        navigator.geolocation.getCurrentPosition(function(pos){
+        navigator.geolocation.getCurrentPosition(function (pos) {
             var lat = pos.coords.latitude;
             var lon = pos.coords.longitude;
             localStorage.setItem('lat', lat);
             localStorage.setItem('lon', lon);
             zCall();
+            initMap(lat, lon);
         });
     } else {
         var cwQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?appid=06e67d1c01fd425c507533b8a4c46d90&q=';
@@ -13,18 +14,21 @@ function search(){
         $.ajax({
             url: cwQueryUrl + document.getElementById('rstInput').value,
             method: 'get'
-        }).then(function(res){
+        }).then(function (res) {
             var lat = res.coord.lat;
             var lon = res.coord.lon;
             localStorage.setItem('lat', lat);
             localStorage.setItem('lon', lon);
             zCall();
+            initMap(lat, lon);
         });
     };
+
+
 };
 
 
-function zCall(){
+function zCall() {
     let zQueryUrl = 'https://developers.zomato.com/api/v2.1/search?lat=' + localStorage.getItem('lat') + '&lon=' + localStorage.getItem('lon') + '&q=vegetarian';
 
     let zomatoAPIKey = '4668fbe6f51718b42874da8ae396ea5c';
@@ -35,32 +39,47 @@ function zCall(){
         type: "GET",
         contentType: "application/json",
         url: zQueryUrl,
-        headers : { "user-key" : zomatoAPIKey }
-    }).then(function(result) {
-        console.log(result)
+        headers: { "user-key": zomatoAPIKey }
+    }).then(function (result) {
+        //console.log(result)
+
+
 
         for (i = 0; i < 18; i++) {
             var srch = result.restaurants[i].restaurant;
 
+
+
             if (srch.featured_image) {
                 var pic = srch.featured_image;
-            } else {    
+            } else {
                 var pic = 'assets/images/ph.jpg';
             }
             var rsts = $("<div class='wrapper col s4 card small'><div class='card-image waves-effect waves-block waves-light'><img class='activator' src='" + pic + "'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>more_vert</i></span><p><a target='_blank' href='" + srch.menu_url + "'>Click here for Menu</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>close</i></span><h6>More Info:</h6><ul><h7>Street Address:</h7><li>" + srch.location.address + "</li><h7>Phone Number:</h7><li>" + srch.phone_numbers + "</li><h7>Customer Rating:</h7><li>" + srch.user_rating.aggregate_rating + "</li></ul></div></div>");
-            
+
             $("#restaurants").append(rsts);
         }
     });
 };
 
-$("button").click(function(){
+function initMap(lat, lon) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: lat, lng: lon },
+        zoom: 8
+    });
+}
+
+$("button").click(function () {
     search();
 });
 
-$('body').keypress(function(e) {
+$('body').keypress(function (e) {
     var keycode = (e.keyCode);
-    if(keycode == '13'){
+    if (keycode == '13') {
         search();
     }
 });
+
+search();
+
+// <iframe src="https://www.google.com/maps/embed/v1/search?key=AIzaSyChMTq7zveGs-pvx0wWFqj9wKGhNcih6_c&q=vegetarian+restaurants+in+Orlando" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
