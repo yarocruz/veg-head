58cedd246881058e12fc8d0ab36cdeabb46d3d68
Antknee <anthonycolindres@yahoo.com>
2020-02-19 14:52:58 -0500

    mobile query

--- assets/restaurantsScript.js
+++ assets/restaurantsScript.js
@@ -1,11 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'top',
-      hoverEnabled: false
-    });
-  });
-  
 function search() {
     if (rstInput.value === "") {
         navigator.geolocation.getCurrentPosition(function (pos) {
@@ -70,7 +62,7 @@ function zCall() {
             } else {
                 var pic = 'assets/images/ph.jpg';
             }
-            var rsts = $("<div class='wrapper col s4 card small'><div class='card-image waves-effect waves-block waves-light'><img class='activator' src='" + pic + "'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>more_vert</i></span><p><a target='_blank' href='" + srch.menu_url + "'>Click here for Menu</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>close</i></span><h6>More Info:</h6><ul><h7>Street Address:</h7><li>" + srch.location.address + "</li><h7>Phone Number:</h7><li>" + srch.phone_numbers + "</li><h7>Customer Rating:</h7><li>" + srch.user_rating.aggregate_rating + "</li></ul></div></div>");
+            var rsts = $("<div id='restCard' class='wrapper col s4 card small'><div id='picEdge' class='card-image waves-effect waves-block waves-light'><img id='resPic' class='activator' src='" + pic + "'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>more_vert</i></span><p><a target='_blank' href='" + srch.menu_url + "'>Click here for Menu</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + srch.name + "<i class='material-icons right'>close</i></span><h6>More Info:</h6><ul><h7>Street Address:</h7><li>" + srch.location.address + "</li><h7>Phone Number:</h7><li>" + srch.phone_numbers + "</li><h7>Customer Rating:</h7><li>" + srch.user_rating.aggregate_rating + "</li></ul></div></div>");
 
             $("#restaurants").append(rsts);
         }
@@ -98,4 +90,6 @@ $('body').keypress(function (e) {
 
 search();
 
-// <iframe src="https://www.google.com/maps/embed/v1/search?key=AIzaSyChMTq7zveGs-pvx0wWFqj9wKGhNcih6_c&q=vegetarian+restaurants+in+Orlando" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+
+  
